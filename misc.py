435e8c69394d697c5ffb6e5c0847824d1f1de5a0
pytera895143242 <91798465+pytera895143242@users.noreply.github.com>
2023-03-14 19:13:13 +0300

    dsd
    sds

---
+++ misc.py
@@ -0,0 +1,12 @@
+import logging
+from aiogram import Bot, Dispatcher
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
+from aiogram.dispatcher.filters.state import State, StatesGroup
+from aiogram.dispatcher import FSMContext
+
+TOKEN = '5976332689:AAGMAO-IE0IgsZ-Hpaw8oMlOGvdtQps_MPA'
+memory_storage = MemoryStorage()
+
+bot = Bot(token=TOKEN,parse_mode='html',disable_web_page_preview=True)
+dp = Dispatcher(bot,storage=memory_storage)
+logging.basicConfig(level=logging.INFO)
